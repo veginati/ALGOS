160a75aa9bccb0c67186232cee1d880b291371c4
vamsiveginati <veginativamsi@gmail.com>
2020-10-12 01:52:34 -0400

    DP With Trees SubOrdinates CSES

--- src/algorithms/dp/trees/SubOrdinates.java
+++ src/algorithms/dp/trees/SubOrdinates.java
@@ -1,38 +1,92 @@
 package algorithms.dp.trees;
 
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
 /**
  * https://cses.fi/problemset/task/1674/
  */
 public class SubOrdinates {
 
-    public static void main(String[] args) {
+    static class Reader
+    {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
 
-        Scanner scanner = new Scanner(System.in);
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
 
-        int n = scanner.nextInt();
-        int[] output = new int[n+1];
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do
+            {
+                ret = ret * 10 + c - '0';
+            }  while ((c = read()) >= '0' && c <= '9');
 
-        List[] neighbours = new ArrayList[n+1];
+            if (neg)
+                return -ret;
+            return ret;
+        }
 
-        for(int i=2;i<=n;i++){
-            int parent = scanner.nextInt();
-            //  neighbours.get(i).add(parent);
-            if(null!= neighbours[parent])
-                neighbours[parent].add(i);
-            else{
-                int value =i;
-                neighbours[parent] = new ArrayList<Integer>(){{add(value);}};
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        //Enter data using BufferReader
+            Reader reader=new Reader();
+            int n = reader.nextInt();
+            int[] output = new int[n+1];
+
+            List[] neighbours = new ArrayList[n+1];
+            for(int i=2;i<=n ;i++){
+                int parent = reader.nextInt();
+                if(null!= neighbours[parent])
+                    neighbours[parent].add(i);
+                else{
+                    int value =i;
+                    neighbours[parent] = new ArrayList<Integer>(){{add(value);}};
+                }
             }
-        }
-        getSubOrdinates(1,output,neighbours);
-        for(int i=1;i<output.length;i++){
-            System.out.print(output[i]);
-            System.out.print(" ");
-        }
+            reader.close();
+            getSubOrdinates(1,output,neighbours);
+            for(int i=1;i<output.length;i++){
+                System.out.print(output[i]);
+                System.out.print(" ");
+            }
     }
 
 
@@ -43,6 +97,8 @@ public class SubOrdinates {
             output[index] =total;
             return;
         }
+        if(output[0]>0)
+            return;
         for(int i=0;i<subOrdinates.size();i++){
             getSubOrdinates(subOrdinates.get(i), output, neighbours);
             total+= 1+ output[subOrdinates.get(i)];
