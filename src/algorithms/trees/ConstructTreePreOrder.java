f018fa9b7b708962e1b569da5a1af3d7993261b7
vamsiveginati <veginativamsi@gmail.com>
2020-11-12 00:40:58 -0500

    LeetCode 1008

---
+++ src/algorithms/trees/ConstructTreePreOrder.java
@@ -0,0 +1,39 @@
+package algorithms.trees;
+
+public class ConstructTreePreOrder {
+    private static int index =0;
+    public static TreeNode bstFromPreorder(int[] preorder) {
+        index=0;
+        return generateBST(preorder,null,null);
+    }
+
+    public static TreeNode generateBST(int[] preorder, Integer min, Integer max){
+
+        if(index>=preorder.length)
+            return null;
+
+        if(null!=min && preorder[index]<min)
+            return null;
+
+        if(null!=max && preorder[index]>max)
+            return null;
+
+        int value = preorder[index++];
+        TreeNode node = new TreeNode(value);
+        node.left = generateBST(preorder,min,value);
+        node.right = generateBST(preorder,value,max);
+
+        return node;
+    }
+
+    public  static void main(String[] args) {
+
+        TreeNode node = bstFromPreorder(new int[]{8,5,1,7,10,12});
+
+        PostOrderTraversalIterator preOrderTraversalIterator = new PostOrderTraversalIterator(node);
+
+        while (preOrderTraversalIterator.hasNext())
+            System.out.println(preOrderTraversalIterator.next().val);
+
+    }
+}
