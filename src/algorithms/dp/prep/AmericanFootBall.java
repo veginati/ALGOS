93194d7954e7a38b9864b84746241819a1f26dfd
vamsiveginati <veginativamsi@gmail.com>
2020-11-22 14:43:32 -0500

    American Football, counting dp, better space complexity O(1).

--- src/algorithms/dp/prep/AmericanFootBall.java
+++ src/algorithms/dp/prep/AmericanFootBall.java
@@ -8,6 +8,12 @@ package algorithms.dp.prep;
 
 public class AmericanFootBall {
 
+    /**
+     * Time is O(n)
+     * Space is O(n)
+     * @param number
+     * @return int
+     */
     public static int findWays(int number){
 
         if(number<2){
@@ -36,9 +42,49 @@ public class AmericanFootBall {
         return scores[number];
     }
 
+
+    /**
+     * Time is O(n)
+     * Space is O(1)
+     * @param number
+     * @return
+     */
+    public static int findWaysII(int number){
+
+        if(number<2){
+            return 0;
+        }
+
+        int[] scores = new int[7];
+
+        //base case, not playing is the base case.
+        scores[0]=1;
+        for(int i=2;i<=number;i++){
+
+            if(i>=2){
+                scores[i%7]=scores[(i-2)%7];
+            }
+
+            if(i>=3){
+                scores[i%7]+=scores[(i-3)%7];
+            }
+
+            if(i>=6){
+                scores[i%7]+=scores[(i-6)%7];
+            }
+        }
+
+        return scores[number%7];
+    }
+
     public static void main(String[] args) {
 
         System.out.println(findWays(8));
         System.out.println(findWays(15));
+        System.out.println(findWays(30));
+        System.out.println("-------------------------------------------------");
+        System.out.println(findWaysII(8));
+        System.out.println(findWaysII(15));
+        System.out.println(findWays(30));
     }
 }
